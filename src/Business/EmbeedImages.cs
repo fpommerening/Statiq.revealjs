5886560d61cacd21fcc65e80b14595cd97eb4f0e
Frank Pommerening <frank@pommerening-online.de>
2021-06-21 21:54:04 +0200

    fix releative path

--- src/Business/EmbeedImages.cs
+++ src/Business/EmbeedImages.cs
@@ -51,7 +51,7 @@ namespace FP.Statiq.RevealJS.Business
             {
                 var baseUrl = context.Settings["baseUrl"].ToString();
                 var sectionPath = document[MetadataKeys.SectionPath].ToString();
-                var path = Path.Combine(baseUrl, sectionPath, src);
+                var path = Path.Combine(baseUrl, Path.GetDirectoryName(sectionPath), src);
                 imageData = await File.ReadAllBytesAsync(path);
             }
 
