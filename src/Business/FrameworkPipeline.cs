314042f10fbaa2c65f5f683d12a9a6fd1e714aac
Frank Pommerening <frank@pommerening-online.de>
2026-08-26 09:36:39 +0200

    Add reveal.js 6.0.1

--- src/Business/FrameworkPipeline.cs
+++ src/Business/FrameworkPipeline.cs
@@ -6,7 +6,7 @@ public class FrameworkPipeline : Pipeline
 {
     public FrameworkPipeline()
     {
-        InputModules.Add(new DownloadGitHub("fpommerening", "presentation", "reveal-js-5.2.1").CacheResponses());
+        InputModules.Add(new DownloadGitHub("fpommerening", "presentation", "reveal-js-6.0.1").CacheResponses());
         ProcessModules.Add(new ExtractZipArchive("presentation"));
         OutputModules.Add(new WriteFiles());
     }
